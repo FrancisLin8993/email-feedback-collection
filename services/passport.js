0e17d1fc813c97be985ce6f6aee4d87c6638238e
FrancisLin1993 <lfc1101@gmail.com>
2019-01-28 11:05:29 +1100

    add passport callback function of dealing with user

--- services/passport.js
+++ services/passport.js
@@ -1,21 +1,31 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const keys = require("../config/keys");
 
-const User = mongoose.model('users');
+const User = mongoose.model("users");
 
 passport.use(
-    new GoogleStrategy(
-      {
-        clientID: keys.googleClientID,
-        clientSecret: keys.googleClientSecret,
-        callbackURL: "/auth/google/callback"
-      },
-      (accessToken, refreshToken, profile, done) => {
-        new User({
-          googleID: profile.id
-        }).save();
-      }
-    )
-  );
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback"
+    },
+    (profile, done) => {
+      User.findOne({ googleID: profile.id }).then(existingUser => {
+        if (existingUser) {
+          done(null, existingUser);
+        } else {
+          new User({
+            googleID: profile.id
+          })
+            .save()
+            .then(user => {
+              done(null, user);
+            });
+        }
+      });
+    }
+  )
+);
