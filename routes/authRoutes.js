3624878d7358cc52dffbac1fb5a1f1128ae13284
FrancisLin1993 <lfc1101@gmail.com>
2019-02-22 10:32:29 +1100

    setup another proxy, initial impl of actions

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -10,12 +10,12 @@ module.exports = app => {
 
   app.get("/auth/google/callback", passport.authenticate("google"));
 
-  app.get("/apis/logout", (req, res) => {
+  app.get("/api/logout", (req, res) => {
     req.logout();
     res.send("Logout sucessfully");
   });
 
-  app.get("/apis/current-user", (req, res) => {
+  app.get("/api/current_user", (req, res) => {
     res.send(req.user);
   });
 };
