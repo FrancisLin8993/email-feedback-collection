f3b2233634b6d6afb4a6f686672b8ed265287931
FrancisLin1993 <lfc1101@gmail.com>
2019-02-22 10:10:59 +1100

    add material-css, basic impl of header

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import Header from './Header';
+import Dashboard from './Dashboard';
+import Landing from './Landing';
+import SurveyNew from './SurveyNew';
 
 const App = () => {
-    return(
-        <div>App</div>
-    );
-}
+  return (
+    <div>
+      <BrowserRouter>
+      <div>
+          <Header />
+          <Route path="/" exact component={Landing} />
+          <Route path="/surveys" exact component={Dashboard} />
+          <Route path="/surveys/new" component={SurveyNew} />
+      </div>
+      </BrowserRouter>
+    </div>
+  );
+};
 
 export default App;
