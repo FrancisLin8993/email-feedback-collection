47d4aaa32633688615272a1f8a89ecbbc4e72dfd
FrancisLin1993 <lfc1101@gmail.com>
2019-02-22 13:27:02 +1100

    implement the auth view and logic of header component

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,20 +1,41 @@
 import React from "react";
+import { connect } from "react-redux";
 
 class Header extends React.Component {
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return;
+      case false:
+        return (
+          <li>
+            <a href="/auth/google">Login with Google</a>
+          </li>
+        );
+      default:
+        return (
+          <li>
+            <a href="/api/logout">Logout</a>
+          </li>
+        );
+    }
+  }
+
   render() {
     return (
       <nav>
         <div className="nav-wrapper">
-          <a href="/" className="left brand-logo">Emailfc</a>
-          <ul className="right">
-            <li>
-              <a href="">Login with Google</a>
-            </li>
-          </ul>
+          <a href="/" className="left brand-logo">
+            Emailfc
+          </a>
+          <ul className="right">{this.renderContent()}</ul>
         </div>
       </nav>
     );
   }
 }
+function mapStateToProps({ auth }) {
+  return { auth };
+}
 
-export default Header;
+export default connect(mapStateToProps)(Header);
