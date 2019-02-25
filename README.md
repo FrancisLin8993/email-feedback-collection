dc8331009d9a02bc784cf0a12113522928519a53
FrancisLin1993 <lfc1101@gmail.com>
2019-02-25 12:44:46 +1100

    add a readme.md

---
+++ README.md
@@ -0,0 +1,1 @@
+This is an app for sending email to others to collect feedbacks.
--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,8 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 class Dashboard extends React.Component {
   render() {
-    return <div>Dashboard</div>;
+    return (
+      <div>
+        <div className="fixed-action-btn">
+          <Link to="/surveys/new" className="btn-floating btn-large red">
+            <i className="material-icons">add</i>
+          </Link>
+        </div>
+      </div>
+    );
   }
 }
 
