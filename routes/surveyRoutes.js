f8ab77507d067ba1570608770172652b55a982c7
FrancisLin1993 <lfc1101@gmail.com>
2019-02-28 09:21:06 +1100

    implement logic of dealing with webhook data and update data in the db

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,13 +1,17 @@
+const _ = require("lodash");
+const Path = require("path-parser");
+const { URL } = require("url");
 const mongoose = require("mongoose");
 const requireLogin = require("../middlewares/requireLogin");
 const requireCredits = require("../middlewares/requireCredits");
 const Mailer = require("../services/Mailer");
 const surveyTemplate = require("../services/emailTemplates/surveyTemplate");
 
+mongoose.Promise = global.Promise;
 const Survey = mongoose.model("surveys");
 
 module.exports = app => {
-  app.get("/api/surveys/thanks", (req, res) => {
+  app.get("/api/surveys/:surveyId/:choice", (req, res) => {
     res.send("Thanks for voting");
   });
 
@@ -35,5 +39,39 @@ module.exports = app => {
     } catch (err) {
       res.status(422).send(err);
     }
+  });
+
+  app.post("/api/surveys/webhooks", (req, res) => {
+    const path = new Path("/api/surveys/:surveyId/:choice");
+
+    _.chain(req.body);
+    _.map(({ email, url }) => {
+      const matchedObject = path.test(new URL(url).pathname);
+      if (matchedObject) {
+        return {
+          email,
+          surveyId: matchedObject.surveyId,
+          choice: matchedObject.choice
+        };
+      }
+    })
+      .compact()
+      .uniqBy("email", "surveyId")
+      .each(({ surveyId, email, choice }) => {
+        Survey.updateOne(
+          {
+            _id: surveyId,
+            recipients: {
+              $elemMatch: { email: email, isResponded: false }
+            }
+          },
+          {
+            $inc: { [choice]: 1 },
+            $set: { "recipients.$.responded": true },
+            lastResponded: new Date()
+          }
+        ).exec();
+      })
+      .value();
   });
 };
