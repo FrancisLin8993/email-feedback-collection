e4393a094f277e1981bb525c5a44e38fbe4697ba
FrancisLin1993 <lfc1101@gmail.com>
2019-03-05 08:48:18 +1100

    modify config about redis

--- services/cache.js
+++ services/cache.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
 const redis = require("redis");
 const util = require("util");
-const redisUrl = "redis://127.0.0.1:6379";
+const keys = require("../config/keys");
+const redisUrl = keys.redisUrl;
 const client = redis.createClient(redisUrl);
 client.hget = util.promisify(client.hget);
 
