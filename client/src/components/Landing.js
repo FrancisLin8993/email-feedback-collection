95e732a0e2cfc5a76d2a484e0db1a537b5073d4a
Francis <flin0002@student.monash.edu>
2019-07-22 22:53:14 +1000

    add simple css of landing component to improve UI

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,10 +1,25 @@
-import React from "react";
+import React from 'react';
 
 class Landing extends React.Component {
   render() {
     return (
-      <div style={{ textAlign: "center" }}>
-        <h1>Emailfc - An App can send emails and collect feedbacks</h1>
+      <div
+        style={{
+          textAlign: 'center',
+          backgroundImage:
+            'url(' +
+            'https://images.unsplash.com/photo-1555421689-491a97ff2040?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&w=1650&q=80' +
+            ')',
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+          backgroundRepeat: 'no-repeat',
+          minHeight: '100vh'
+        }}
+      >
+        <h1 style={{ marginTop: '0' }}>Emailfc</h1>
+        <h5 style={{ bottom: '0' }}>
+          An App can send emails and collect feedbacks
+        </h5>
       </div>
     );
   }
