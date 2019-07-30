c3521b4ba1cb842b370243721a0cf5925437ad37
Francis <flin0002@student.monash.edu>
2019-07-30 17:46:01 +1000

    integrate with travisci

---
+++ config/ci.js
@@ -0,0 +1,11 @@
+module.exports = {
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  mongoURI: 'mongodb://127.0.0.1:27017/emailfc',
+  cookieKey: process.env.COOKIE_KEY,
+  stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
+  stripeSecretKey: process.env.STRIPE_SECRET_KEY,
+  sendGridKey: process.env.SEND_GRID_KEY,
+  redirectDomain: process.env.REDIRECT_DOMAIN,
+  redisUrl: 'redis://127.0.0.1:6379'
+};
--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,8 @@
 //decide which crendentials to return
-if (process.env.NODE_ENV === 'production'){
-    module.exports = require('./prod');
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./prod');
+} else if (process.env.NODE_ENV === 'ci') {
+  module.exports = require('./ci');
 } else {
-    module.exports = require('./dev');
+  module.exports = require('./dev');
 }
